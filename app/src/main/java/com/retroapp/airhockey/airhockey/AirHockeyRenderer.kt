6a7ad15d2d69e6a39b85439e0423c8aab65cfebd
Toufique Imam <2013nuhash@gmail.com>
2023-04-01 23:50:43 +0600

    7.6 Drawing Our Texture

--- app/src/main/java/com/retroapp/airhockey/airhockey/AirHockeyRenderer.kt
+++ app/src/main/java/com/retroapp/airhockey/airhockey/AirHockeyRenderer.kt
@@ -6,97 +6,36 @@ import android.opengl.GLSurfaceView
 import android.opengl.Matrix
 import android.opengl.Matrix.*
 import com.retroapp.airhockey.R
-import com.retroapp.airhockey.airhockey.util.LoggerConfig
-import com.retroapp.airhockey.airhockey.util.MatrixHelper
-import com.retroapp.airhockey.airhockey.util.ShaderHelper
-import com.retroapp.airhockey.airhockey.util.TextResourceReader
+import com.retroapp.airhockey.airhockey.objects.Mallet
+import com.retroapp.airhockey.airhockey.objects.Table
+import com.retroapp.airhockey.airhockey.programs.ColorShaderProgram
+import com.retroapp.airhockey.airhockey.programs.TextureShaderProgram
+import com.retroapp.airhockey.airhockey.util.*
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 import kotlin.math.PI
 
 
 class AirHockeyRenderer(private val context: Context) : GLSurfaceView.Renderer {
-    private val positionComponentCount = 4
-    private val aPosition = "a_Position"
-    private val uMatrix = "u_Matrix"
-    private var aPositionLocation: Int = 0
-
-    private val colorComponentCount = 3
-    private val aColor = "a_Color"
-    private var aColorLocation = 0
-    private val stride = (positionComponentCount + colorComponentCount) * ShaderHelper.bytesPerFloat
-    private val tableVerticesWithTriangles = floatArrayOf(
-        // Order of coordinates: X, Y, Z, W, R, G, B
-
-        // Triangle Fan
-        0f, 0f, 0f, 1.5f, 1f, 1f, 1f,
-        -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-        0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-        0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
-        -0.5f, 0.8f, 0f, 2f, 0.7f, 0.7f, 0.7f,
-        -0.5f, -0.8f, 0f, 1f, 0.7f, 0.7f, 0.7f,
-
-        // Line 1
-        -0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
-        0.5f, 0f, 0f, 1.5f, 1f, 0f, 0f,
-
-        // Mallets
-        0f, -0.4f, 0f, 1.25f, 0f, 0f, 1f,
-        0f, 0.4f, 0f, 1.75f, 1f, 0f, 0f
-    )
     private val projectionMatrix = FloatArray(16)
     private val modelMatrix = FloatArray(16)
-    private var uMatrixLocation = 0
 
+    lateinit var table : Table
+    lateinit var mallet : Mallet
+    lateinit var textureShaderProgram : TextureShaderProgram
+    lateinit var colorShaderProgram : ColorShaderProgram
+    private var texture = 0
 
-    private val vertexData = ShaderHelper.toFloatBuffer(tableVerticesWithTriangles)
-    private var program: Int = 0
+    override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
+        glClearColor(0f,0f,0f,0f)
+        table = Table()
+        mallet = Mallet()
+        textureShaderProgram = TextureShaderProgram(context)
+        colorShaderProgram = ColorShaderProgram(context)
 
-    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
-        //Reading in the Shader Code
-        val vertexShaderSource = TextResourceReader
-            .readTextFileFromResource(context, R.raw.simple_vertex_shader)
-        val fragmentShaderSource = TextResourceReader
-            .readTextFileFromResource(context, R.raw.simple_fragment_shader)
-        //Compiling the Shaders from Our Renderer Class
-        val vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource)
-        val fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource)
-        //Linking Shaders Together into an OpenGL Program
-        program = ShaderHelper.linkProgram(vertexShader, fragmentShader)
-        //Validate Our OpenGL Program Object
-        ShaderHelper.validateProgram(program)
-        glUseProgram(program)
-        //Getting the Location of a Uniform
-        aColorLocation = glGetAttribLocation(program, aColor)
-        //Getting the Location of an Attribute
-        aPositionLocation = glGetAttribLocation(program, aPosition)
-        uMatrixLocation = glGetUniformLocation(program, uMatrix)
-        //Associating an Array of Vertex Data with an Attribute
-        vertexData.position(0)
-        glVertexAttribPointer(
-            aPositionLocation,
-            positionComponentCount,
-            GL_FLOAT,
-            false,
-            stride,
-            vertexData
-        )
-        //Enabling the Vertex Array
-        glEnableVertexAttribArray(aPositionLocation)
+        texture = TextureHelper.loadTexture(context, R.drawable.air_hockey_surface)
 
-        vertexData.position(positionComponentCount)
-        glVertexAttribPointer(
-            aColorLocation,
-            colorComponentCount,
-            GL_FLOAT,
-            false,
-            stride,
-            vertexData
-        )
-        glEnableVertexAttribArray(aColorLocation)
     }
-
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         glViewport(0, 0, width, height)
         val aspectRatio =
@@ -115,20 +54,18 @@ class AirHockeyRenderer(private val context: Context) : GLSurfaceView.Renderer {
         System.arraycopy(temp, 0, projectionMatrix, 0, temp.size)
     }
 
-    override fun onDrawFrame(gl: GL10?) {
+    override fun onDrawFrame(p0: GL10?) {
         glClear(GL_COLOR_BUFFER_BIT)
+        textureShaderProgram.useProgram()
+        textureShaderProgram.setUniforms(projectionMatrix, texture)
 
-        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0)
-        //Drawing the Table
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 6)
+        table.bindData(textureShaderProgram)
+        table.draw()
 
-        // Draw the center dividing line.
-        glDrawArrays(GL_LINES, 6, 2)
-
-        // Draw the first mallet.
-        glDrawArrays(GL_POINTS, 8, 1)
-
-        // Draw the second mallet.
-        glDrawArrays(GL_POINTS, 9, 1)
+        colorShaderProgram.useProgram()
+        colorShaderProgram.setUniforms(projectionMatrix)
+        mallet.bindData(colorShaderProgram)
+        mallet.draw()
     }
+
 }
