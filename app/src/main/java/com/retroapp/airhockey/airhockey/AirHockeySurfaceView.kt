25a543fcea857b5a24e17ae9a0a7703f72c66b5c
Toufique Imam <2013nuhash@gmail.com>
2023-04-09 02:26:55 +0600

    8.6 Integrating Our Changes

--- app/src/main/java/com/retroapp/airhockey/airhockey/AirHockeySurfaceView.kt
+++ app/src/main/java/com/retroapp/airhockey/airhockey/AirHockeySurfaceView.kt
@@ -11,7 +11,7 @@ class AirHockeySurfaceView(context: Context) : GLSurfaceView(context) {
     private var previousY: Float = 0f
 
     init {
-        setEGLContextClientVersion(2);
+        setEGLContextClientVersion(2)
 
         renderer = AirHockeyRenderer(context)
         //setEGLConfigChooser(8, 8, 8, 8, 16, 0);
@@ -24,7 +24,7 @@ class AirHockeySurfaceView(context: Context) : GLSurfaceView(context) {
         // MotionEvent reports input details from the touch screen
         // and other input controls. In this case, you are only
         // interested in events where the touch position changed.
-        if (event == null) return false;
+        if (event == null) return false
         val x: Float = event.x
         val y: Float = event.y
         when (event.action) {
